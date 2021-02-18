cfec7ed68695df3981d7aef294ed28d963c80a22
Durgesh Agnihotri <45296392+durgeshiv@users.noreply.github.com>
2021-02-19 01:44:33 +0530

    Update exercise1.scala

--- src/main/scala/com/durgeshSoftware/scalaImplicits/exercise1.scala
+++ src/main/scala/com/durgeshSoftware/scalaImplicits/exercise1.scala
@@ -8,7 +8,16 @@ object exercise1 extends App{
 
   case class Person(name: String, age: Int)
   val lst = List(Person("Marry", 25), Person("Alwyn", 40), Person("Chris", 31))
-  implicit def nameAlphbeticalOrder: Ordering[Person] = Ordering.fromLessThan((a,b) => a.name < b.name)
+  //implicit def nameAlphbeticalOrder: Ordering[Person] = Ordering.fromLessThan((a,b) => a.name < b.name)
+  //val sortedList = lst.sorted
+  //println(sortedList)
+
+  /**
+   * Q2. Lets order a List persons as defined below on basis of their age, decreasing order.
+   */
+
+
+  implicit def ageReverseOrder: Ordering[Person] = Ordering.fromLessThan((a,b) => a.age > b.age)
   val sortedList = lst.sorted
   println(sortedList)
 }
