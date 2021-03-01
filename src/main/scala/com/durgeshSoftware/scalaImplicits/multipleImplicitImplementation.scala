77aa68d6869c4fb6920d4cc2920f47746d24aa80
Durgesh Agnihotri <45296392+durgeshiv@users.noreply.github.com>
2021-03-02 03:40:11 +0530

    multipleImplicitImplementationComplete

--- src/main/scala/com/durgeshSoftware/scalaImplicits/multipleImplicitImplementation.scala
+++ src/main/scala/com/durgeshSoftware/scalaImplicits/multipleImplicitImplementation.scala
@@ -1,12 +1,27 @@
 package com.durgeshSoftware.scalaImplicits
 
-object multipleImplicitImplementation {
+import com.durgeshSoftware.scalaImplicits.placingImplicits.Person
 
+/**
+ * Written by Durgesh Agnihotri - 1614636351
+ */
+object multipleImplicitImplementation extends App {
+
+  // defining same list as previously
+  val lst = List(Person("Marry", 25), Person("Alwyn", 40), Person("Chris", 31))
+
+  // implementing implicit by importing it, to order list by age ::
+
+  import implicitDef2._
+  val sortedList = lst.sorted
+  println(sortedList)
 
 }
 
 object implicitDef1 {
+  implicit def nameAlphbeticalOrder: Ordering[Person] = Ordering.fromLessThan((a,b) => a.name < b.name)
+}
 
-
-
+object implicitDef2 {
+  implicit def ageReverseOrder: Ordering[Person] = Ordering.fromLessThan((a,b) => a.age > b.age)
 }
